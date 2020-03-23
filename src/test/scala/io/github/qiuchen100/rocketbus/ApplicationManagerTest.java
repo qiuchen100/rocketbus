428f665229f12274ca349ebac7f8ad21e3b1726b
qiuchen <qiuchen100@163.com>
2020-03-24 00:29:46 +0800

    部分功能设计中。

---
+++ src/test/scala/io/github/qiuchen100/rocketbus/ApplicationManagerTest.java
@@ -0,0 +1,13 @@
+package io.github.qiuchen100.rocketbus;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationManagerTest {
+
+    @Test
+    public void main() {
+
+    }
+}
